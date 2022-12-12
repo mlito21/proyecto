cfdbe2b59823f37a72a2c2879427cbf80e43afc2
Marlon Maldonado <mailito21@gmail.com>
2022-12-12 16:55:25 -0500

    Add files via upload

---
+++ dashboard/dashboard.js
@@ -0,0 +1,52 @@
+/* globals Chart:false, feather:false */
+
+(() => {
+  'use strict'
+
+  feather.replace({ 'aria-hidden': 'true' })
+
+  // Graphs
+  const ctx = document.getElementById('myChart')
+  // eslint-disable-next-line no-unused-vars
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: [
+        'Semana 1',
+        'Semana 2',
+        'Semana 3',
+        'Semana 4',
+        'Semana 5',
+        'Semana 6',
+      ],
+      datasets: [{
+        data: [
+          10,
+          7,
+          5,
+          8,
+          9,
+          4,
+          9.5
+        ],
+        lineTension: 0,
+        backgroundColor: 'transparent',
+        borderColor: '#007bff',
+        borderWidth: 4,
+        pointBackgroundColor: '#007bff'
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: false
+          }
+        }]
+      },
+      legend: {
+        display: false
+      }
+    }
+  })
+})()
